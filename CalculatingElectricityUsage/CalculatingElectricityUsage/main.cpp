884a680cce3da6654762d0dd060a19eb6cd41c77
huylg <huylg@vng.com.vn>
2020-03-11 22:33:32 +0700

    calculating electricity usage

---
+++ CalculatingElectricityUsage/CalculatingElectricityUsage/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  CalculatingElectricityUsage
+//
+//  Created by Huy Ly on 3/11/20.
+//  Copyright © 2020 HuyLg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int a,b;
+    std::cin>>a>>b;
+    std::cout<<b-a;
+    return 0;
+}
