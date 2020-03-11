dd7fcdf49ae9b9a5aab191fccec2e2ebf917bca0
huylg <huylg@vng.com.vn>
2020-03-11 22:21:28 +0700

    Calculate BMI

--- CalculateBMI/CalculateBMI/main.cpp
+++ CalculateBMI/CalculateBMI/main.cpp
@@ -1,23 +1,15 @@
 //
 //  main.cpp
-//  CalculateVolumeOfARectangleBox
+//  CalculateBMI
 //
 //  Created by Huy Ly on 3/11/20.
 //  Copyright © 2020 HuyLg. All rights reserved.
 //
 
 #include <iostream>
-using namespace std;
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    
-    int result = 1;
-    for(int i=0;i<3;i++)
-    {
-        int a;
-        cin>>a;
-        result*=a;
-    }
-    cout<<result;
+    std::cout << "Hello, World!\n";
     return 0;
 }
