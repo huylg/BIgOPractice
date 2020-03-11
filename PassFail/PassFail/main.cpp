ea772df88452cf9d14525bfe70b540a04702184b
huylg <huylg@vng.com.vn>
2020-03-11 21:57:01 +0700

    PassFail practice

---
+++ PassFail/PassFail/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PassFail
+//
+//  Created by Huy Ly on 3/11/20.
+//  Copyright © 2020 HuyLg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
