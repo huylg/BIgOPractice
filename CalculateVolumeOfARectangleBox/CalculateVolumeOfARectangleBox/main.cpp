d91ed8f9d4a9b6dbb3bf2ab70557443bb83771d9
huylg <huylg@vng.com.vn>
2020-03-11 22:12:43 +0700

    Calculate volume of a rectangle box

---
+++ CalculateVolumeOfARectangleBox/CalculateVolumeOfARectangleBox/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CalculateVolumeOfARectangleBox
+//
+//  Created by Huy Ly on 3/11/20.
+//  Copyright © 2020 HuyLg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
